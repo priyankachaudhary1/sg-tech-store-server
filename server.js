9461eb04b77cc850c449ec4bdb8174933d3c80ae
sanjayguwaju <sanjayguwaju@gmail.com>
2023-08-26 23:18:24 +0545

    tech-store repo initialized

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected successfully!');
+  } catch (error) {
+    console.error('Error connecting to database:', error.message);
+  }
+}
+
+connectToDatabase();
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'Server is up and running' });
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
+});
