beeae75c23f3ded8f2c09500adb1f5ea4f61efc2
sanjayguwaju <sanjayguwaju@gmail.com>
2023-09-17 11:32:19 +0545

    fix: added server setup

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-// Required imports
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRoutes = require('./routes/auth');
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+require("dotenv").config();
 
-// Middelwares
+// app
 const app = express();
-app.use(express.json());
-app.use(morgan('dev'));
-app.use(cors());
-app.use('/api/auth', authRoutes);
-
 
 // Conntection to the Database
 async function connectToDatabase() {
@@ -29,15 +24,21 @@ async function connectToDatabase() {
 
 connectToDatabase();
 
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
 // Routes
 app.get('/healthcheck', (req, res) => {
-    res.status(200).json({ message: 'Server is up and running' });
-  });
-
-
-
-
-// Start the server
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port ${process.env.PORT}`);
+  res.status(200).json({ message: 'Server is up and running' });
 });
+
+// routes middleware
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
