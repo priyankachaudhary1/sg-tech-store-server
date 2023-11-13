6d746c22047aee898d9be5784ad5b21048883fe5
sanjayguwaju <sanjayguwaju@gmail.com>
2023-11-14 00:15:52 +0545

    fix: login process done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,16 @@
 const admin = require("../firebase");
 
-exports.authCheck = (req, res, next) => {
-  console.log(req.headers); // token
-  next();
+exports.authCheck = async (req, res, next) => {
+  try {
+    const firebaseUser = await admin
+    .auth()
+    .verifyIdToken(req?.headers?.authToken);
+    console.log("FIREBASE USER IN AUTHCHECK", firebaseUser);
+    req.user = firebaseUser; // Question" Is firebaseUser is saved to req.user although in request user was not present.
+    next();
+  } catch (error) {
+    res.status(401).json({
+      err: "Invalid or expired token"
+    })
+  }
 };
