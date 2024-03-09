a466121d96041ee1052d145ba2abea4c2d665b7f
sanjayguwaju <sanjayguwaju@gmail.com>
2024-03-09 08:18:41 +0545

    fixed: middleware path

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // middlewares
-const { authCheck, adminCheck } = require("../controllers/cloudinary");
+const { authCheck, adminCheck } = require("../middlewares/auth");
 
 // controllers
 const { upload, remove } = require("../controllers/cloudinary");
