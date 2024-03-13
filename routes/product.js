89880607ae8482614f301fee5339342466691f65
sanjayguwaju <sanjayguwaju@gmail.com>
2024-03-13 13:11:34 +0545

    products count api and pagination completed

--- routes/product.js
+++ routes/product.js
@@ -5,9 +5,18 @@ const router = express.Router();
 const { authCheck, adminCheck } = require("../middlewares/auth");
 
 // controller
-const { create, read, listAll, remove, update, list} = require("../controllers/product");
+const { 
+    create,
+    read,
+    listAll, 
+    remove, 
+    update, 
+    list, 
+    productsCount
+} = require("../controllers/product");
 
 // routes
+router.get("/products/total", productsCount);
 router.post("/product", authCheck, adminCheck, create);
 router.get("/products/:count", listAll);
 router.delete("/product/:slug", authCheck, adminCheck, remove);
