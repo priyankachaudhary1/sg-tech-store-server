52c98286bb9098c7f3dced59f5b343ff09f6dd40
sanjayguwaju <sanjayguwaju@gmail.com>
2024-03-18 20:01:10 +0545

    show related products

--- controllers/product.js
+++ controllers/product.js
@@ -158,3 +158,23 @@ exports.productStar = async (req, res) => {
     });
   }
 };
+
+exports.listRelated = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.productId).exec();
+    const related = await Product.find({
+      _id: { $ne: product._id },
+      category: product.category,
+    })
+      .limit(3)
+      .populate("category")
+      .populate("subs")
+      .populate("ratings.postedBy")
+      .exec();
+    res.json(related);
+  } catch (err) {
+    res.status(500).json({
+      err: err.message,
+    });
+  }
+};
--- routes/product.js
+++ routes/product.js
@@ -14,6 +14,7 @@ const {
     list, 
     productsCount,
     productStar,
+    listRelated,
 } = require("../controllers/product");
 
 // routes
@@ -27,5 +28,6 @@ router.post("/products", list);
 
 // rating
 router.put("/product/star/:productId", authCheck, productStar);
+router.get("/product/related/:productId", listRelated);
 
 module.exports = router;
